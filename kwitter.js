05a39fdfe6fb6883c78cee40a983f0ca46bef687
nyshitha619 <80595384+nyshitha619@users.noreply.github.com>
2021-12-26 11:44:21 -0600

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,38 @@
+
+
+function addUser() {
+    user_name = document.getElementById("user_name").value;
+    given_code = document.getElementById("password").value;
+    room_name = given_code;
+    
+    localStorage.setItem("user_name", user_name);
+    localStorage.setItem("room_name", room_name);
+    window.location = "kwitter_page.html";
+    
+}
+
+
+
+function addParent() {
+
+    user_name = "Adult";
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "kwitter_room.html"
+}
+
+function parent() {
+    window.location = "parentsCreate.html"
+}
+
+function HowTo(){
+    window.location = "parents_room.html"
+}
+
+function logout() {
+    console.log(name);
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+    window.location = "index.html"
+}
